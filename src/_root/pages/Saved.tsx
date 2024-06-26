78076f9e4d15ec781448b1c10d1d482df05aebce
Ravi <ravibh7284@gmail.com>
2024-06-26 23:01:33 +0530

    version 1.5

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -36,7 +36,7 @@ const Saved = () => {
             {savePosts.length === 0 ? (
               <p className="text-light-4">No available posts</p>
             ) : (
-              <GridPostList posts={savePosts} showStats={false} />
+              <GridPostList posts={savePosts} showUser={false} showStats={false} />
             )}
           </ul>
         )}
